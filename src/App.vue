1196ff5a1653e1d877d8d692a273793884e762d2
Fernando Diez <dzfsoft@gmail.com>
2023-11-26 07:34:57 +0100

    Another title

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! Fernando Diez sos un genio" />
+      <HelloWorld msg="You did it! Lo dejo mejor como estaba al principio" />
     </div>
   </header>
 
